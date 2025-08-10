673e2877e2671e9871f0d48c6b4943726044af0d
李铭聪 <2058615288@qq.com>
2025-08-10 15:34:09 +0800

    home-面板组件的封装

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -4,18 +4,26 @@ import HomeCategory from './components/HomeCategory.vue';
 import HomeHot from './components/HomeHot.vue';
 import HomeNew from './components/HomeNew.vue';
 import HomeProduct from './components/HomeProduct.vue';
+import HomePanel from './components/HomePanel.vue';
 
 
 </script>
 
 <template>
   <div class="container">
-    <HomeCategory/>
+    <HomeCategory />
     <HomeBanner />
   </div>
-  <HomeNew/>
+  <HomeNew />
   <HomeHot />
   <HomeProduct />
+
+  <HomePanel title="LMC" sub-title="lmc">
+    <HomeNew />
+  </HomePanel>
+  <HomePanel title="WHS" sub-title="whs">
+    <HomeProduct />
+  </HomePanel>
 </template>
 
 <style >
