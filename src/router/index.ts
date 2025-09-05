865bcb2e1a0abb7cab2668d1b9bab034e636cdbb
LiMingCong <2058615288@qq.com>
2025-09-05 18:34:55 +0800

    支付结果的展示

--- src/router/index.ts
+++ src/router/index.ts
@@ -37,6 +37,10 @@ const router = createRouter({
           path: "pay",
           component: () => import("@/views/Pay/index.vue"),
         },
+        {
+          path: "payback",
+          component: () => import("@/views/Pay/PayBack.vue"),
+        },
       ],
     },
     {
